c845da6398c0963e135e3d5dbd6fd1901a68e832
David Evans <david0evans@gmail.com>
2026-01-31 15:09:55 +0000

    add barn prop

--- ggj-2026-unity/Assets/Game/Scripts/GameController.cs
+++ ggj-2026-unity/Assets/Game/Scripts/GameController.cs
@@ -149,18 +149,18 @@ public class GameController : Singleton<GameController>
   {
     switch (newState)
     {
-    case eGameState.Intro:
-      ShowUI<MainMenuUI>();
-      AudioManager.Instance.PlaySound(MusicTitle);
-      break;
-    case eGameState.Game:
-      SpawnLevel();
-      AudioManager.Instance.PlaySound(MusicGame);
-      break;
-    case eGameState.PostGame:
-      ShowUI<PostGameUI>();
-      AudioManager.Instance.PlaySound(MusicEnd);
-      break;
+      case eGameState.Intro:
+        ShowUI<MainMenuUI>();
+        AudioManager.Instance.PlaySound(MusicTitle);
+        break;
+      case eGameState.Game:
+        SpawnLevel();
+        AudioManager.Instance.PlaySound(MusicGame);
+        break;
+      case eGameState.PostGame:
+        ShowUI<PostGameUI>();
+        AudioManager.Instance.PlaySound(MusicEnd);
+        break;
     }
   }
 
@@ -168,19 +168,19 @@ public class GameController : Singleton<GameController>
   {
     switch (oldState)
     {
-    case eGameState.Intro:
-      HideUI<MainMenuUI>();
-      AudioManager.Instance.StopSound(MusicTitle);
-      break;
-    case eGameState.Game:
-      //HideUI<CountdownTimerUI>();
-      AudioManager.Instance.StopSound(MusicGame);
-      break;
-    case eGameState.PostGame:
-      ClearLevel();
-      HideUI<PostGameUI>();
-      AudioManager.Instance.StopSound(MusicEnd);
-      break;
+      case eGameState.Intro:
+        HideUI<MainMenuUI>();
+        AudioManager.Instance.StopSound(MusicTitle);
+        break;
+      case eGameState.Game:
+        //HideUI<CountdownTimerUI>();
+        AudioManager.Instance.StopSound(MusicGame);
+        break;
+      case eGameState.PostGame:
+        ClearLevel();
+        HideUI<PostGameUI>();
+        AudioManager.Instance.StopSound(MusicEnd);
+        break;
     }
   }
 
@@ -248,7 +248,7 @@ public class GameController : Singleton<GameController>
 
       if (playerSpawnPoint != null)
       {
-        var spawnTransform= playerSpawnPoint.transform;
+        var spawnTransform = playerSpawnPoint.transform;
 
         SpawnPlayerAtLocation(playerIndex, spawnTransform.position, spawnTransform.rotation);
       }
