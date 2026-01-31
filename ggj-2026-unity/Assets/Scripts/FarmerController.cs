c9a552aed9374714400cf9384f228fcb130d92aa
Brendan Walker <brendan@millerwalker.net>
2026-01-31 22:09:23 +0000

    Fixing 4-space to 2-space tabs in FarmerController

--- ggj-2026-unity/Assets/Scripts/FarmerController.cs
+++ ggj-2026-unity/Assets/Scripts/FarmerController.cs
@@ -6,205 +6,205 @@ using UnityEngine.UIElements;
 
 public interface IState
 {
-    void OnEnter(FarmerController controller);
-    void UpdateState(FarmerController controller);
-    void OnExit(FarmerController controller);
+  void OnEnter(FarmerController controller);
+  void UpdateState(FarmerController controller);
+  void OnExit(FarmerController controller);
 }
 
 public class FarmerController : MonoBehaviour
 {
-    public float speed = 10;
-    public float turnSpeed = 45;
+  public float speed = 10;
+  public float turnSpeed = 45;
 
-    public float minProximityToTarget = 5;
+  public float minProximityToTarget = 5;
 
-    public float minIdleTime = 5;
-    public float maxIdleTime = 10;
+  public float minIdleTime = 5;
+  public float maxIdleTime = 10;
 
-    IState currentState;
+  IState currentState;
 
-    GameObject[] targets;
+  GameObject[] targets;
 
-    List<GameObject> shuffledTargets = new List<GameObject>();
-    ObjectActorController _actor= null;
+  List<GameObject> shuffledTargets = new List<GameObject>();
+  ObjectActorController _actor = null;
 
-    public GameObject currentTarget = null;
+  public GameObject currentTarget = null;
 
-    void Start()
+  void Start()
+  {
+    _actor = GetComponent<ObjectActorController>();
+
+    targets = GameObject.FindGameObjectsWithTag("FarmerTarget");
+    ShuffleTargets();
+    ChangeState(new WalkState());
+  }
+
+  void Update()
+  {
+    if (shuffledTargets.Count <= 0)
     {
-        _actor= GetComponent<ObjectActorController>();
-
-        targets = GameObject.FindGameObjectsWithTag("FarmerTarget");
-        ShuffleTargets();
-        ChangeState(new WalkState());
+      ShuffleTargets();
     }
 
-    void Update()
+    if (currentState != null)
     {
-        if(shuffledTargets.Count <= 0)
-        {
-            ShuffleTargets();
-        }
+      currentState.UpdateState(this);
+    }
+  }
 
-        if (currentState != null)
-        {
-            currentState.UpdateState(this);
-        }
+  public void ChangeState(IState newState)
+  {
+    if (currentState != null)
+    {
+      currentState.OnExit(this);
+    }
+    currentState = newState;
+    currentState.OnEnter(this);
+  }
+
+  public void ShuffleTargets()
+  {
+    for (int targetIndex = 0; targetIndex < targets.Length; targetIndex++)
+    {
+      shuffledTargets.Add(targets[targetIndex]);
     }
 
-    public void ChangeState(IState newState)
+
+    System.Random random = new System.Random();
+    int n = shuffledTargets.Count;
+    for (int i = 0; i < n; i++)
     {
-        if (currentState != null)
-        {
-            currentState.OnExit(this);
-        }
-        currentState = newState;
-        currentState.OnEnter(this);
+      int r = i + random.Next(n - i);
+      GameObject temp = shuffledTargets[r];
+      shuffledTargets[r] = shuffledTargets[i];
+      shuffledTargets[i] = temp;
     }
+  }
 
-    public void ShuffleTargets()
+  //public void ShuffleTargets()
+  //{
+  //    List<int> targetIndexes = new List<int>();
+
+  //    for (int targetIndex = 0; targetIndex < targets.Length; targetIndex++)
+  //    {
+  //        targetIndexes.Add(targetIndex); 
+  //    }
+
+  //    for (int shufflerIndex = 0; shufflerIndex < targets.Length; shufflerIndex++)
+  //    {
+  //        int index = Random.Range(0, targetIndexes.Count);
+  //        shuffledTargets.Add(targets[index]);
+  //        targetIndexes.Remove(index);
+  //    }
+  //}
+
+  public void FindAndSetNextTarget()
+  {
+    currentTarget = shuffledTargets[0];
+    shuffledTargets.Remove(currentTarget);
+  }
+
+  public void MoveTowards(Vector3 direction)
+  {
+    if (_actor != null)
     {
-        for (int targetIndex = 0; targetIndex < targets.Length; targetIndex++)
-        {
-            shuffledTargets.Add(targets[targetIndex]);
-        }
-
-
-        System.Random random = new System.Random();
-        int n = shuffledTargets.Count;
-        for (int i = 0; i < n; i++)
-        {
-            int r = i + random.Next(n - i);
-            GameObject temp = shuffledTargets[r];
-            shuffledTargets[r] = shuffledTargets[i];
-            shuffledTargets[i] = temp;
-        }
+      _actor.MoveAxis = direction.XZ();
     }
-
-    //public void ShuffleTargets()
-    //{
-    //    List<int> targetIndexes = new List<int>();
-
-    //    for (int targetIndex = 0; targetIndex < targets.Length; targetIndex++)
-    //    {
-    //        targetIndexes.Add(targetIndex); 
-    //    }
-
-    //    for (int shufflerIndex = 0; shufflerIndex < targets.Length; shufflerIndex++)
-    //    {
-    //        int index = Random.Range(0, targetIndexes.Count);
-    //        shuffledTargets.Add(targets[index]);
-    //        targetIndexes.Remove(index);
-    //    }
-    //}
-
-    public void FindAndSetNextTarget()
+    else
     {
-        currentTarget = shuffledTargets[0];
-        shuffledTargets.Remove(currentTarget);
-    }
-
-    public void MoveTowards(Vector3 direction)
-    {
-      if (_actor != null)
+      // if not facing target, turn towards it
+      if (direction != -transform.forward)
       {
-        _actor.MoveAxis = direction.XZ();
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed); // Smooth rotation
       }
-      else
-      {
-        // if not facing target, turn towards it
-        if (direction != -transform.forward)
-        {
-          Quaternion targetRotation = Quaternion.LookRotation(direction);
-          transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed); // Smooth rotation
-        }
 
-        // if we made it this far, move toward the target
-        transform.position += direction * speed * Time.deltaTime;
-      }
+      // if we made it this far, move toward the target
+      transform.position += direction * speed * Time.deltaTime;
     }
+  }
 }
 
 public class WalkState : IState
 {
-    public void OnEnter(FarmerController controller)
+  public void OnEnter(FarmerController controller)
+  {
+    controller.FindAndSetNextTarget();
+  }
+
+  public void UpdateState(FarmerController controller)
+  {
+    // if close to target, change to idle
+    float distanceToTarget = Vector3.Distance(controller.currentTarget.transform.position, controller.transform.position);
+
+    if (distanceToTarget <= controller.minProximityToTarget)
     {
-        controller.FindAndSetNextTarget();
+      controller.ChangeState(new IdleState());
+      return;
     }
 
-    public void UpdateState(FarmerController controller)
+    // something something perceive a player
+    bool fakebool = false;
+    if (fakebool)
     {
-        // if close to target, change to idle
-        float distanceToTarget = Vector3.Distance(controller.currentTarget.transform.position, controller.transform.position);
-
-        if (distanceToTarget <= controller.minProximityToTarget)
-        {
-            controller.ChangeState(new IdleState());
-            return;
-        }
-
-        // something something perceive a player
-        bool fakebool = false;
-        if (fakebool)
-        {
-            controller.ChangeState(new StartledState());
-        }
-
-
-        Vector3 heading = controller.currentTarget.transform.position - controller.transform.position;
-        Vector3 direction = heading.NormalizedSafe();
-
-        controller.MoveTowards(direction);
+      controller.ChangeState(new StartledState());
     }
 
-    public void OnExit(FarmerController controller)
-    {
-        // Cleanup patrol state
-        controller.MoveTowards(Vector3.zero);
-    }
+
+    Vector3 heading = controller.currentTarget.transform.position - controller.transform.position;
+    Vector3 direction = heading.NormalizedSafe();
+
+    controller.MoveTowards(direction);
+  }
+
+  public void OnExit(FarmerController controller)
+  {
+    // Cleanup patrol state
+    controller.MoveTowards(Vector3.zero);
+  }
 }
 
 public class StartledState : IState
 {
-    public void OnEnter(FarmerController controller)
-    {
-        // Initialize chase state
-    }
+  public void OnEnter(FarmerController controller)
+  {
+    // Initialize chase state
+  }
 
-    public void UpdateState(FarmerController controller)
-    {
-        // Chase logic
-    }
+  public void UpdateState(FarmerController controller)
+  {
+    // Chase logic
+  }
 
-    public void OnExit(FarmerController controller)
-    {
-        // Cleanup chase state
-    }
+  public void OnExit(FarmerController controller)
+  {
+    // Cleanup chase state
+  }
 }
 
 public class IdleState : IState
 {
-    float timeRemaining = 1000;
+  float timeRemaining = 1000;
 
-    public void OnEnter(FarmerController controller)
+  public void OnEnter(FarmerController controller)
+  {
+    timeRemaining = Random.Range(controller.minIdleTime, controller.maxIdleTime);
+  }
+
+  public void UpdateState(FarmerController controller)
+  {
+    if (timeRemaining > 0)
     {
-        timeRemaining = Random.Range(controller.minIdleTime, controller.maxIdleTime);
+      timeRemaining -= Time.deltaTime;
+      if (timeRemaining < 0)
+      {
+        controller.ChangeState(new WalkState());
+      }
     }
+  }
 
-    public void UpdateState(FarmerController controller)
-    {
-        if (timeRemaining > 0)
-        {
-            timeRemaining -= Time.deltaTime;
-            if(timeRemaining < 0)
-            {
-                controller.ChangeState(new WalkState());
-            }
-        }
-    }
+  public void OnExit(FarmerController controller)
+  {
 
-    public void OnExit(FarmerController controller)
-    {
-
-    }
+  }
 }
