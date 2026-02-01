71ce1e2867f52aa3d294c9fe0bb549a4b602b533
Brendan Walker <brendan@millerwalker.net>
2026-02-01 11:17:18 +0000

    Null check the perception object

--- ggj-2026-unity/Assets/Scripts/FarmerController.cs
+++ ggj-2026-unity/Assets/Scripts/FarmerController.cs
@@ -110,7 +110,7 @@ public class FarmerController : MonoBehaviour
 
   public bool IsStartled()
   {
-    return perceptionObject.GetDetectedObjectCount() > 0;
+    return perceptionObject ? perceptionObject.GetDetectedObjectCount() > 0 : false;
   }
 
   public void ShuffleTargets()
