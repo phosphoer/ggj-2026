ce958726e454ee915e68d5a832a3e77c3edd3783
David Evans <david0evans@gmail.com>
2026-02-01 15:59:20 +0000

    idk

---
+++ push-itch.sh
@@ -0,0 +1,3 @@
+~/Library/'Application Support'/itch/broth/butler/versions/15.21.0/butler push ./build/soil-toil-mac stickybiscuits/soiltoil:osx
+~/Library/'Application Support'/itch/broth/butler/versions/15.21.0/butler push ./build/soil-toil-windows stickybiscuits/soiltoil:windows
+#~/Library/'Application Support'/itch/broth/butler/versions/15.21.0/butler push ggj-2026-unity/build/soiltoil-linux stickybiscuits/soiltoil:linux
