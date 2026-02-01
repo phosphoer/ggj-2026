69b9f27bd1baf0ff06444111bb6209c47d7b4165
David Evans <david0evans@gmail.com>
2026-02-01 00:48:43 +0000

    - hose attack - spook hit box startles farmer

--- ggj-2026-unity/Assets/Scripts/SpookHitBox.cs
+++ ggj-2026-unity/Assets/Scripts/SpookHitBox.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class SpookHitBox : MonoBehaviour
 {
+  private bool _hitConsumed = false;
 
+  private void OnTriggerEnter(Collider c)
+  {
+    if (_hitConsumed)
+      return;
+
+    FarmerController farmer = c.GetComponent<FarmerController>();
+    if (farmer)
+    {
+      farmer.PlayEmote(FarmerController.eEmote.startled);
+      _hitConsumed = true;
+    }
+  }
 }
