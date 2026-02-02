f5e211beda30937fdc26932b3577afd479f7f647
keyp3r <marshall_parsons@hotmail.com>
2026-02-02 14:18:37 +0000

    Dead farmer ragdoll
    Took some doing, but it works and it's insane.
    Also updated the Scarecrow FX to be world space. They were local.
    AND fixed the fruit tree attack location. It was too high to hit the farmer.

---
+++ ggj-2026-unity/Assets/Scripts/ParentConstraint.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityGLTF.Interactivity.VisualScripting.Export;
+
+public class ParentConstraint : MonoBehaviour
+{
+
+    public bool DefaultEnabled = true;
+
+    public Transform Target;
+
+    public bool Constrain_Position = true;
+    public bool Constrain_Rotation = true;
+    public bool MaintainOffset = true;
+    public bool UpdateOffsetOnEnable = false;
+
+    bool constrain = true;
+    Vector3 position_offset = Vector3.zero;
+    Vector3 euler_offset = Vector3.zero;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SetOffset();
+        SetConstraint(DefaultEnabled);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(constrain && Target!=null)
+        {
+            if(Constrain_Position) transform.position = Target.position + position_offset;
+            if(Constrain_Rotation) transform.eulerAngles = Target.eulerAngles + euler_offset;
+        }
+    }
+
+    public void SetConstraint(bool constraint_enabled)
+    {
+        if(UpdateOffsetOnEnable) SetOffset();
+
+        constrain = constraint_enabled;
+    }
+
+    public void SetOffset()
+    {
+        if(MaintainOffset)
+        {
+            position_offset = transform.position - Target.position;
+            euler_offset = transform.eulerAngles - Target.eulerAngles;
+        }
+    }
+
+}
